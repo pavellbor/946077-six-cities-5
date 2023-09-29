912840511f58dfe98a9cf46f68b7b1b1fba1b3dc
Pavel Borisov <pavellbor@gmail.com>
2023-09-29 10:55:39 +0300

    feat: Добавлен запрос на получение данных. WIP

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,15 +1,34 @@
+import got from "got";
+import { MockServerData } from "../../shared/types/mock-server-data.type.js";
 import { Command } from "./command.interface.js";
 
 export class GenerateCommand implements Command {
-  getName(): string {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
+  public getName(): string {
     return "--generate";
   }
 
-  execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
-    // Код для получения данных с сервера.
-    // Формирование объявлений.
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.log("Can't generate data");
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
