90f4ca8764a51151063062268d4d26adee8cb02b
Pavel Borisov <pavellbor@gmail.com>
2023-09-30 10:56:02 +0300

    chore: Рефакторинг GenerateCommand

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -2,7 +2,8 @@ import got from "got";
 import { MockServerData } from "../../shared/types/index.js";
 import { Command } from "./command.interface.js";
 import { TSVOfferGenerator } from "../../shared/libs/offer-generator/index.js";
-import { appendFile } from "fs/promises";
+import { TSVFileWriter } from "../../shared/libs/file-writer/index.js";
+import { getErrorMessage } from "../../shared/helpers/common.js";
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
@@ -17,10 +18,10 @@ export class GenerateCommand implements Command {
 
   private async write(filepath: string, offerCount: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath)
+
     for (let i = 0; i < offerCount; i++) {
-      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
-        encoding: "utf-8",
-      });
+      await tsvFileWriter.write(tsvOfferGenerator.generate())
     }
   }
 
@@ -37,11 +38,8 @@ export class GenerateCommand implements Command {
       await this.write(filepath, offerCount);
       console.info(`File ${filepath} was created!`)
     } catch (error: unknown) {
-      console.log("Can't generate data");
-
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
+      console.error("Can't generate data");
+      console.error(getErrorMessage(error))
     }
   }
 }
