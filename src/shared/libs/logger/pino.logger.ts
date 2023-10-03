ad6f2e953bad284cc46d75482632aa6241046ecb
Pavel Borisov <pavellbor@gmail.com>
2023-10-03 22:14:09 +0300

    Добавит пример с мультитранспортом (файлы, терминал)

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -11,12 +11,22 @@ export class PinoLogger implements Logger {
     const logFilePath = "logs/rest.log";
     const destination = resolve(modulePath, "../../../", logFilePath);
 
-    const fileTransport = transport({
-      target: "pino/file",
-      options: { destination },
+    const multiTransport = transport({
+      targets: [
+        {
+          target: "pino/file",
+          options: { destination },
+          level: "debug",
+        },
+        {
+          target: "pino/file",
+          options: {},
+          level: "info",
+        },
+      ],
     });
 
-    this.logger = pino({}, fileTransport);
+    this.logger = pino({}, multiTransport);
   }
 
   public debug(message: string, ...args: unknown[]): void {
