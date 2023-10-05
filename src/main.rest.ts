2394d28c9b8ad3e5893d4f9f3db946cfe55f97be
Pavel Borisov <pavellbor@gmail.com>
2023-10-05 10:19:59 +0300

    Настроит контейнер.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -7,9 +7,9 @@ import { Config, RestConfig, RestSchema } from "./shared/libs/config/index.js";
 
 async function bootstrap() {
   const container = new Container();
-  container.bind<RestApplication>(Component.RestApplicaton).to(RestApplication);
-  container.bind<Logger>(Component.Logger).to(PinoLogger);
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig);
+  container.bind<RestApplication>(Component.RestApplicaton).to(RestApplication).inRequestScope()
+  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplicaton);
   await application.init();
