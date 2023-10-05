19f1756a038e69f105437ceaae6e99d2c76dc171
Pavel Borisov <pavellbor@gmail.com>
2023-10-05 10:03:01 +0300

    Добавит контейнер с зависимостями. WIP

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,12 +1,17 @@
+import "reflect-metadata";
+import { Container } from "inversify";
 import { RestApplication } from "./rest/index.js";
-import { RestConfig } from "./shared/libs/config/index.js";
-import { PinoLogger } from "./shared/libs/logger/index.js";
+import { Component } from "./shared/types/index.js";
+import { Logger, PinoLogger } from "./shared/libs/logger/index.js";
+import { Config, RestConfig, RestSchema } from "./shared/libs/config/index.js";
 
 async function bootstrap() {
-  const logger = new PinoLogger();
-  const config = new RestConfig(logger)
+  const container = new Container();
+  container.bind<RestApplication>(Component.RestApplicaton).to(RestApplication);
+  container.bind<Logger>(Component.Logger).to(PinoLogger);
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig);
 
-  const application = new RestApplication(logger, config);
+  const application = container.get<RestApplication>(Component.RestApplicaton);
   await application.init();
 }
 
