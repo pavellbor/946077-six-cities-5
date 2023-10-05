85346292148a6429f5d49102fe427d1972d46599
Pavel Borisov <pavellbor@gmail.com>
2023-10-05 09:25:46 +0300

    Обновит класс RestConfig.

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,22 +1,28 @@
-import { DotenvParseOutput, config } from "dotenv";
+import { config } from "dotenv";
 import { Logger } from "../logger/index.js";
 import { Config } from "./config.interface.js";
+import { RestSchema, configRestSchema } from "./rest.schema.js";
 
-export class RestConfig implements Config {
-  private readonly config: NodeJS.ProcessEnv
+export class RestConfig implements Config<RestSchema> {
+  private readonly config: RestSchema;
 
   constructor(private readonly logger: Logger) {
-    const parsedOutput = config()
+    const parsedOutput = config();
 
     if (parsedOutput.error) {
-      throw new Error('Can\'t read .env file. Perhaps the file does not exists.')
+      throw new Error(
+        "Can't read .env file. Perhaps the file does not exists."
+      );
     }
 
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
-    this.logger.info('.env file found and successfully parsed!');
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: "strict", output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
+    this.logger.info(".env file found and successfully parsed!");
   }
 
-  get(key: string): string | undefined {
+  get<T extends keyof RestSchema>(key: T): RestSchema[T] {
     return this.config[key];
   }
 }
