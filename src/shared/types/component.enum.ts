21df4cb394b0f1724a6f6d9348b09df4e642beea
Pavel Borisov <pavellbor@gmail.com>
2023-10-08 22:04:14 +0300

    Добавит новый компонент в Component.

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -1,5 +1,6 @@
 export const Component = {
-  RestApplicaton: Symbol.for('RestApplication'),
-  Logger: Symbol.for('Logger'),
-  Config: Symbol.for('Config'),
+  RestApplicaton: Symbol.for("RestApplication"),
+  Logger: Symbol.for("Logger"),
+  Config: Symbol.for("Config"),
+  DatabaseClient: Symbol.for("DatabaseClient"),
 } as const;
