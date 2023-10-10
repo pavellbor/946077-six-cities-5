ff1ee74fcf673914217e56397fd924113a0f622a
Pavel Borisov <pavellbor@gmail.com>
2023-10-10 21:19:38 +0300

    Добавит имплементацию методов UserService.

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -9,8 +9,9 @@ import { Logger } from "../../libs/logger/index.js";
 export class DefaultUserService implements UserService {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
-    @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>
-    ) {}
+    @inject(Component.UserModel)
+    private readonly userModel: types.ModelType<UserEntity>
+  ) {}
 
   public async create(
     dto: CreateUserDto,
@@ -23,5 +24,24 @@ export class DefaultUserService implements UserService {
     this.logger.info(`New user created: ${user.email}`);
 
     return result;
+  }
+
+  public async findByEmail(
+    email: string
+  ): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({ email });
+  }
+
+  public async findOrCreate(
+    dto: CreateUserDto,
+    salt: string
+  ): Promise<DocumentType<UserEntity>> {
+    const existedUser = await this.findByEmail(dto.email);
+
+    if (existedUser) {
+      return existedUser;
+    }
+
+    return this.create(dto, salt);
   }
 }
