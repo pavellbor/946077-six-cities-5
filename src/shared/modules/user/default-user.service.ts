e5b9bd63bbbec2304703d34853fe4ee25e31ccb8
Pavel Borisov <pavellbor@gmail.com>
2023-10-10 18:32:58 +0300

    Добавит внедрение зависимостей в DefaultUserService. WIP.

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,8 +1,14 @@
 import { DocumentType } from "@typegoose/typegoose";
 import { CreateUserDto, UserEntity, UserModel } from "./index.js";
 import { UserService } from "./user-service.interface.js";
+import { inject, injectable } from "inversify";
+import { Component } from "../../types/component.enum.js";
+import { Logger } from "../../libs/logger/index.js";
 
+@injectable()
 export class DefaultUserService implements UserService {
+  constructor(@inject(Component.Logger) private readonly logger: Logger) {}
+
   public async create(
     dto: CreateUserDto,
     salt: string
@@ -10,6 +16,9 @@ export class DefaultUserService implements UserService {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    return UserModel.create(UserEntity);
+    const result = await UserModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+
+    return result;
   }
 }
