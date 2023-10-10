232cd84d0b95438e0f55bcd6cd3ce24c46af3b40
Pavel Borisov <pavellbor@gmail.com>
2023-10-10 16:56:24 +0300

    Добавит поля _id и id. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,6 +1,7 @@
 import { defaultClasses, getModelForClass, prop } from "@typegoose/typegoose";
 import { User, UserType } from "../../types/index.js";
 
+export interface UserEntity extends defaultClasses.Base {}
 export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: true, minlength: 1, maxlength: 15 })
   public name: string = "";
