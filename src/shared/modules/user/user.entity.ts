14277812620d24cb1800ef725f5dde4f824f05d6
Pavel Borisov <pavellbor@gmail.com>
2023-10-10 17:33:25 +0300

    Добавит методы в UserEntity. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,19 +1,51 @@
-import { defaultClasses, getModelForClass, prop } from "@typegoose/typegoose";
+import {
+  defaultClasses,
+  getModelForClass,
+  modelOptions,
+  prop,
+} from "@typegoose/typegoose";
 import { User, UserType } from "../../types/index.js";
+import { createSHA256 } from "../../helpers/index.js";
 
 export interface UserEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: "users",
+  },
+})
 export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: true, minlength: 1, maxlength: 15 })
-  public name: string = "";
+  public name: string;
 
   @prop({ required: false, default: "" })
-  public avatarUrl: string = "";
+  public avatarUrl: string;
 
   @prop({ unique: true, required: true })
-  public email: string = "";
+  public email: string;
+
+  @prop({ required: true, default: UserType.Standart })
+  public type: UserType;
 
   @prop({ required: true })
-  public type: UserType = UserType.Standart;
+  private password?: string;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.name = userData.name;
+    this.avatarUrl = userData.avatarUrl;
+    this.type = userData.type;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
