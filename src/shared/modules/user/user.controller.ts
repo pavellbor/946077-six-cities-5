8d72add467792cd6aae82dec8d803db90aa8dbc2
Pavel Borisov <pavellbor@gmail.com>
2023-11-12 16:37:13 +0300

    Добавит создание пользователей в UserController.

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,13 +1,27 @@
 import { inject, injectable } from 'inversify';
-import { BaseController, HttpMethod } from '../../libs/rest/index.js';
+import {
+  BaseController,
+  HttpError,
+  HttpMethod,
+} from '../../libs/rest/index.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
-import { NextFunction, Response } from 'express';
+import { Response } from 'express';
 import { CreateUserRequest } from './create-user-request.type.js';
+import { UserService } from './index.js';
+import { StatusCodes } from 'http-status-codes';
+import { Config, RestSchema } from '../../libs/config/index.js';
+import { fillDTO } from '../../helpers/index.js';
+import { UserRdo } from './rdo/user.rdo.js';
 
 @injectable()
 export class UserController extends BaseController {
-  constructor(@inject(Component.Logger) protected readonly logger: Logger) {
+  constructor(
+    @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.Config)
+    private readonly configService: Config<RestSchema>,
+    @inject(Component.UserService) private readonly userService: UserService
+  ) {
     super(logger);
 
     this.logger.info('Register routes for UserController...');
@@ -20,10 +34,23 @@ export class UserController extends BaseController {
   }
 
   public async create(
-    _req: CreateUserRequest,
-    _res: Response,
-    _next: NextFunction
+    { body }: CreateUserRequest,
+    res: Response
   ): Promise<void> {
-      throw new Error('[UserController] Oops');
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» exists.`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(
+      body,
+      this.configService.get('SALT')
+    );
+    this.created(res, fillDTO(UserRdo, result));
   }
 }
