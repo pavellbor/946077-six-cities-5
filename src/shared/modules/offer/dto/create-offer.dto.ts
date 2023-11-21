55f9d0dfd1eef6d8e1fc67780c22aec15616b711
Pavel Borisov <pavellbor@gmail.com>
2023-11-21 09:39:36 +0300

    Добавит валидаторы для CreateOfferDto.

--- src/shared/modules/offer/dto/create-offer.dto.ts
+++ src/shared/modules/offer/dto/create-offer.dto.ts
@@ -1,23 +1,83 @@
 import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsBoolean,
+  IsDateString,
+  IsEnum,
+  IsInt,
+  IsMongoId,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+import {
   City,
   GoodsType,
   HousingType,
   Location,
 } from '../../../types/index.js';
+import { CreateOfferValidationMessage } from './create-offer-validation-message.js';
 
 export class CreateOfferDto {
+  @MinLength(10, { message: CreateOfferValidationMessage.title.minLength })
+  @MaxLength(100, { message: CreateOfferValidationMessage.title.maxLength })
   public title: string;
+
+  @MinLength(20, {
+    message: CreateOfferValidationMessage.description.minLength,
+  })
+  @MaxLength(1024, {
+    message: CreateOfferValidationMessage.description.maxLength,
+  })
   public description: string;
+
+  @IsDateString(
+    {},
+    { message: CreateOfferValidationMessage.postDate.invalidFormat }
+  )
   public postDate: Date;
+
+  @IsEnum(City, { message: CreateOfferValidationMessage.city.invalid })
   public city: City;
+
+  @MaxLength(256, {
+    message: CreateOfferValidationMessage.previewImage.maxLength,
+  })
   public previewImage: string;
+
+  @IsArray({ message: CreateOfferValidationMessage.images.invalidFormat })
+  @ArrayMinSize(6)
+  @ArrayMaxSize(6)
   public images: string[];
+
+  @IsBoolean({ message: CreateOfferValidationMessage.isPremium.invalidFormat })
   public isPremium: boolean;
+
+  @IsEnum(HousingType, { message: CreateOfferValidationMessage.type.invalid })
   public type: HousingType;
+
+  @IsInt({ message: CreateOfferValidationMessage.bedrooms.invalidFormat })
+  @Min(1, { message: CreateOfferValidationMessage.bedrooms.minValue })
+  @Max(8, { message: CreateOfferValidationMessage.bedrooms.maxValue })
   public bedrooms: number;
+
+  @IsInt({ message: CreateOfferValidationMessage.maxAdults.invalidFormat })
+  @Min(1, { message: CreateOfferValidationMessage.maxAdults.minValue })
+  @Max(10, { message: CreateOfferValidationMessage.maxAdults.maxValue })
   public maxAdults: number;
+
+  @IsInt({ message: CreateOfferValidationMessage.price.invalidFormat })
+  @Min(100, { message: CreateOfferValidationMessage.price.minValue })
+  @Max(100000, { message: CreateOfferValidationMessage.price.maxValue })
   public price: number;
+
+  @IsArray({ message: CreateOfferValidationMessage.goods.invalidFormat })
   public goods: GoodsType[];
+
+  @IsMongoId({ message: CreateOfferValidationMessage.hostId.invalidId })
   public hostId: string;
+
   public location: Location;
 }
