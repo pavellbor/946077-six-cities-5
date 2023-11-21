964c777e4f331168eed0b479d329bd2200981d06
Pavel Borisov <pavellbor@gmail.com>
2023-11-21 22:27:32 +0300

    Добавит middleware для проверки наличия документа в коллекции.

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -11,3 +11,4 @@ export * from './errors/index.js';
 export * from './middleware/middleware.interface.js';
 export * from './middleware/validate-objectid.middleware.js';
 export * from './middleware/validate-dto.middleware.js';
+export * from './middleware/document-exists.middleware.js';
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -8,3 +8,4 @@ export * from './user.type.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
 export * from './sort-type.enum.js';
+export * from './document-exists.interface.js';
