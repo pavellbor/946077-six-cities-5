1d6baab314325d632012467bb3cd455455e062de
Pavel Borisov <pavellbor@gmail.com>
2023-12-10 20:17:19 +0300

    Добавит AuthExceptionFilter. WIP

---
+++ src/shared/modules/auth/auth.exception-filter.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import { ExceptionFilter } from '../../libs/rest/index.js';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/component.enum.js';
+import { Logger } from '../../libs/logger/logger.interface.js';
+import { BaseUserException } from './errors/base-user.exception.js';
+
+@injectable()
+export class AuthExceptionFilter implements ExceptionFilter {
+  constructor(@inject(Component.Logger) private readonly logger: Logger) {
+    this.logger.info('Register AuthExceptionFilter');
+  }
+
+  public catch(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void {
+    if (!(error instanceof BaseUserException)) {
+      return next(error);
+    }
+
+    this.logger.error(`[AuthModule] ${error.message}`, error);
+    res.status(error.httpStatusCode).json({
+      type: 'AUTHORIZATION',
+      error: error.message,
+    });
+  }
+}
