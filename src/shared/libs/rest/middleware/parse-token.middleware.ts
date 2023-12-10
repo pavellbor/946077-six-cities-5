86983c85aa49b0b096fef2a50624d69d55d8db9c
Pavel Borisov <pavellbor@gmail.com>
2023-12-10 21:51:40 +0300

    Добавит ParseTokenMiddleware. WIP

---
+++ src/shared/libs/rest/middleware/parse-token.middleware.ts
@@ -0,0 +1,55 @@
+import { Request, Response, NextFunction } from 'express';
+import { Middleware } from './middleware.interface.js';
+import { jwtVerify } from 'jose';
+import { createSecretKey } from 'crypto';
+import { TokenPayload } from '../../../modules/auth/index.js';
+import { HttpError } from '../index.js';
+import { StatusCodes } from 'http-status-codes';
+
+function isTokenPayload(payload: unknown): payload is TokenPayload {
+  return (
+    typeof payload === 'object' &&
+    payload !== null &&
+    'email' in payload &&
+    typeof payload.email === 'string' &&
+    'type' in payload &&
+    typeof payload.type === 'string' &&
+    'id' in payload &&
+    typeof payload.id === 'string'
+  );
+}
+
+export class ParseTokenMiddleware implements Middleware {
+  constructor(private readonly jwtSecret: string) {}
+
+  public async execute(
+    req: Request,
+    _res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const authorizationHeader = req.headers?.authorization?.split(' ');
+    if (!authorizationHeader) {
+      return next();
+    }
+
+    const [, token] = authorizationHeader;
+
+    try {
+      const { payload } = await jwtVerify(
+        token,
+        createSecretKey(this.jwtSecret, 'utf-8')
+      );
+      if (isTokenPayload(payload)) {
+        req.tokenPayload = { ...payload };
+      }
+    } catch {
+      return next(
+        new HttpError(
+          StatusCodes.UNAUTHORIZED,
+          'Invalid token',
+          'AuthenticateMiddleware'
+        )
+      );
+    }
+  }
+}
